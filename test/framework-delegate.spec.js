6432de86ae802f00d9fa70be248e23a7bb80a359
Michael Tintiuc <contact@michaeltintiuc.com>
2018-07-31 22:49:09 +0300

    Add framework delegation tests

---
+++ test/framework-delegate.spec.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import { Delegate } from '../src/framework-delegate.js'
+
+describe('Framwork Delegation', () => {
+  it('Attaches to DOM', () => {
+    expect.assertions(2)
+    const component = Vue.component('foo', {
+      props: { foo: { default: '', type: String } },
+      template: '<div>foo</div>',
+    })
+
+    return Delegate.attachViewToDom(document.body, component, { foo: 'bar' }, ['foo']).then(el => {
+      expect(el.classList.contains('foo')).toBeTruthy()
+      expect(el.__vue__.foo).toBe('bar')
+      return
+    })
+  })
+
+  it('Removes from DOM', () => {
+    expect.assertions(1)
+    const div = document.querySelector('div')
+    return Delegate.removeViewFromDom(document.body, div).then(() => {
+      return expect(div.__vue__).toBe(null)
+    })
+  })
+})
