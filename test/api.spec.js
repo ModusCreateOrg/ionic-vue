7798319f07f0e42873606cf33a9b5529a9f20144
Michael Tintiuc <contact@michaeltintiuc.com>
2018-07-31 23:45:11 +0300

    Streamline API testing

--- test/api.spec.js
+++ test/api.spec.js
@@ -1,6 +1,20 @@
 import Vue from 'vue'
 import API from '../src/api.js'
 
+beforeEach(() => {
+  HTMLElement.prototype.componentOnReady = function() {
+    const el = this
+    el.create = function(props = {}) {
+      return Object.assign(el, props)
+    }
+    return Promise.resolve(el)
+  }
+})
+
+afterEach(() => {
+  HTMLElement.prototype.componentOnReady = undefined
+})
+
 describe('API', () => {
   it('Installs correctly', () => {
     Vue.use(API)
@@ -13,55 +27,33 @@ describe('API', () => {
 
   it('Creates nav controller', () => {
     expect.assertions(1)
-    mockComponentOnReady()
 
     return API.newNavController('foo').then(c => {
-      expect(c.root).toBe('foo')
-      return releaseComponentOnReady()
+      return expect(c.root).toBe('foo')
     })
   })
 
   it('Creates alert controllers', () => {
     expect.assertions(3)
-    mockComponentOnReady()
 
     // Creates initial element
     API.newAlertController({ foo: 'bar' })
       .then(c => {
-        expect(c.foo).toBe('bar')
-        return releaseComponentOnReady()
+        return expect(c.foo).toBe('bar')
       })
       .catch(err => err)
 
     // Returns previous element with extra props
     return API.newAlertController({ bar: 'foo' }).then(c => {
       expect(c.foo).toBe('bar')
-      expect(c.bar).toBe('foo')
-      return releaseComponentOnReady()
+      return expect(c.bar).toBe('foo')
     })
   })
 
   it('Creates loading controllers', done => {
-    mockComponentOnReady()
-
     return API.newLoadingController({ bar: 'foo' }).then(c => {
       expect(c.bar).toBe('foo')
-      releaseComponentOnReady()
       return done()
     })
   })
 })
-
-function mockComponentOnReady() {
-  HTMLElement.prototype.componentOnReady = function() {
-    const el = this
-    el.create = function(props = {}) {
-      return Object.assign(el, props)
-    }
-    return Promise.resolve(el)
-  }
-}
-
-function releaseComponentOnReady() {
-  HTMLElement.prototype.componentOnReady = undefined
-}
