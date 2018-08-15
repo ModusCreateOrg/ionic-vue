4914ca5a96f09ff879bc7fc38a48b63b07f7b3cb
Michael Tintiuc <michaeltintiuc@users.noreply.github.com>
2018-08-16 00:47:38 +0300

    Allow custom transitions (#18)
    * Allow custom Vue transitions

--- src/components/ion-vue-router.vue
+++ src/components/ion-vue-router.vue
@@ -20,8 +20,11 @@
 </template>
 
 <script>
+import catchIonicGoBack from '../mixins/catch-ionic-go-back.js'
+
 export default {
   name: 'IonVueRouter',
+  mixins: [catchIonicGoBack],
   props: {
     // A name to call "named views" by
     name: {
@@ -58,29 +61,6 @@ export default {
     })
   },
   methods: {
-    // Catch the bubbled-up event from the Ionic's back button
-    catchIonicGoBack(event) {
-      // We only care for the event coming from Ionic's back button
-      const backButton = event.target && event.target.closest('ion-back-button')
-      if (!backButton) return
-
-      let defaultHref
-
-      // Explicitly override router direction
-      // This will always trigger a back transition
-      this.$router.directionOverride = -1
-
-      // If we can go back - do so
-      // otherwise if there's a default fall-back - use it
-      // else - skip
-      if (this.$router.canGoBack()) {
-        event.preventDefault()
-        this.$router.back()
-      } else if (undefined !== (defaultHref = backButton.defaultHref)) {
-        event.preventDefault()
-        this.$router.push(defaultHref)
-      }
-    },
     transition(enteringEl, leavingEl) {
       // Get the reference to the Ionic component handling the transitions
       const ionRouterOutlet = this.$refs.ionRouterOutlet
