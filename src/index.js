ee26b5d3413854f5abbc384d04bcad570857bceb
Michael Tintiuc <contact@michaeltintiuc.com>
2018-08-18 16:09:16 +0300

    Update ionic init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-export { default as IonicInit } from './ionic'
+export { default as Ionic } from './ionic'
 export { default as IonicAPI } from './api'
 export { default as IonicVueRouter } from './router'
