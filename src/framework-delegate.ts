0c8a3472012a54dccf713aa9ea605b7b2794692c
Michael Tintiuc <contact@michaeltintiuc.com>
2018-10-06 21:38:42 +0300

    Tslint fixes

--- src/framework-delegate.ts
+++ src/framework-delegate.ts
@@ -1,75 +1,75 @@
-import { Vue, VueConstructor } from 'vue/types/vue'
-import { HTMLVueElement, FrameworkDelegate, WebpackFunction } from './types/interfaces'
+import { Vue, VueConstructor } from 'vue/types/vue';
+import { FrameworkDelegate, HTMLVueElement, WebpackFunction } from './types/interfaces';
 
 export default class Delegate implements FrameworkDelegate {
   constructor(public Vue: VueConstructor) {}
 
   // Attach the passed Vue component to DOM
-  attachViewToDom(parentElement: HTMLElement, component: HTMLElement|WebpackFunction|object|Vue, opts?: object, classes?: string[]): Promise<HTMLElement> {
+  attachViewToDom(parentElement: HTMLElement, component: HTMLElement | WebpackFunction | object | Vue, opts?: object, classes?: string[]): Promise<HTMLElement> {
     // Handle HTML elements
     if (isElement(component)) {
       // Add any classes to the element
-      addClasses(component as HTMLElement, classes)
+      addClasses(component as HTMLElement, classes);
 
       // Append the element to DOM
-      parentElement.appendChild(component as HTMLElement)
-      return Promise.resolve(component as HTMLElement)
+      parentElement.appendChild(component as HTMLElement);
+      return Promise.resolve(component as HTMLElement);
     }
 
     // Get the Vue controller
     return this.vueController(component).then(controller => {
-      const vueComponent = this.vueComponent(controller, opts)
+      const vueComponent = this.vueComponent(controller, opts);
 
       // Add any classes to the Vue component's root element
-      addClasses(vueComponent.$el, classes)
+      addClasses(vueComponent.$el, classes);
 
       // Append the Vue component to DOM
-      parentElement.appendChild(vueComponent.$el)
-      return vueComponent.$el
-    })
+      parentElement.appendChild(vueComponent.$el);
+      return vueComponent.$el;
+    });
   }
 
   // Remove the earlier created Vue component from DOM
   removeViewFromDom(_parentElement: HTMLElement, childElement: HTMLVueElement): Promise<void> {
     // Destroy the Vue component instance
     if (childElement.__vue__) {
-      childElement.__vue__.$destroy()
+      childElement.__vue__.$destroy();
     }
 
-    return Promise.resolve()
+    return Promise.resolve();
   }
 
   // Handle creation of sync and async components
-  vueController(component: WebpackFunction|object|Vue) {
+  vueController(component: WebpackFunction | object | Vue) {
     return Promise.resolve(
       typeof component === 'function' && (component as WebpackFunction).cid === undefined
       ? (component as WebpackFunction)().then((c: any) => this.Vue.extend(isESModule(c) ? c.default : c))
         : this.Vue.extend(component)
-    )
+    );
   }
 
   // Create a new instance of the Vue component
   vueComponent(controller: VueConstructor, opts?: object) {
-    return new controller(opts).$mount()
+    return new controller(opts).$mount();
   }
 }
 
 // Check Symbol support
-const hasSymbol = typeof Symbol === 'function' && typeof Symbol.toStringTag === 'symbol'
+const hasSymbol = typeof Symbol === 'function' && typeof Symbol.toStringTag === 'symbol';
 
 // Check if object is an ES module
 function isESModule(obj: any) {
-  return obj.__esModule || (hasSymbol && obj[Symbol.toStringTag] === 'Module')
+  return obj.__esModule || (hasSymbol && obj[Symbol.toStringTag] === 'Module');
 }
 
 // Check if value is an Element
 function isElement(el: any) {
-  return typeof Element !== 'undefined' && el instanceof Element
+  return typeof Element !== 'undefined' && el instanceof Element;
 }
 
 // Add an array of classes to an element
 function addClasses(element: HTMLElement, classes: string[] = []) {
   for (const cls of classes) {
-    element.classList.add(cls)
+    element.classList.add(cls);
   }
 }
--- src/types/interfaces.d.ts
+++ src/types/interfaces.d.ts
@@ -1,20 +1,20 @@
-import Vue from 'vue'
+import Vue from 'vue';
 
 export interface HTMLVueElement extends HTMLElement {
-  __vue__: Vue
+  __vue__: Vue;
 }
 
 export interface WebpackFunction extends Function {
-  cid: number
+  cid: number;
 }
 
 export interface HTMLStencilElement extends HTMLElement {
-  componentOnReady(): Promise<this>
-  componentOnReady(done: (el?: this) => void): void
-  forceUpdate(): void
+  componentOnReady(): Promise<this>;
+  componentOnReady(done: (el?: this) => void): void;
+  forceUpdate(): void;
 }
 
 export interface FrameworkDelegate {
-  attachViewToDom(parentElement: HTMLElement, component: (HTMLElement|Function|object|Vue), opts?: (object|null), classes?: string[]): Promise<HTMLElement>
-  removeViewFromDom(parentElement: HTMLElement, childElement: HTMLVueElement): Promise<void>
+  attachViewToDom(parentElement: HTMLElement, component: HTMLElement | WebpackFunction | object | Vue, opts?: object, classes?: string[]): Promise<HTMLElement>;
+  removeViewFromDom(parentElement: HTMLElement, childElement: HTMLVueElement): Promise<void>;
 }
