711014450f67225d0093491a90b42510025bce55
Tim Vu <timvu@timvuc0tc9pg3qd.lan>
2020-05-28 09:59:49 -0400

    configuration edits for TS unit tests

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-  testURL: 'http://localhost/',
-  moduleFileExtensions: ['js', 'vue'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node', 'vue'],
   moduleNameMapper: {
     '^vue$': 'vue/dist/vue.common.js',
   },
   transform: {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
     '.*\\.(vue)$': '<rootDir>/node_modules/jest-vue-preprocessor',
+    '^.+\\.tsx?$': 'ts-jest'
   },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   "testResultsProcessor": "jest-sonar-reporter"
 }
