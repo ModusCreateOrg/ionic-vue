260c26835452ed8e7d3d0f9dd2fa9640d67199fa
Tim Vu <timvu@Tim-Vu-C02NTC9PG3QD.local>
2020-06-02 10:20:02 -0400

    treeshaking w/ webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,        
+            },
+        ],
+    },
+    mode: 'production',
+    optimization: {
+        usedExports: true,
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
