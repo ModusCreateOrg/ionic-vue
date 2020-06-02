643700721fa3ab306d46fe1029e1d7444d251611
Tim Vu <timvu@Tim-Vu-C02NTC9PG3QD.local>
2020-06-02 11:45:58 -0400

    added terser configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
     entry: './src/index.ts',
@@ -13,7 +14,10 @@ module.exports = {
     },
     mode: 'production',
     optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
         usedExports: true,
+
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
