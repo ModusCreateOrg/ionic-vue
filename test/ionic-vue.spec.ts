dc8d4de9588190cff2d55c158e689bcfffd644a4
Tim Vu <timvu@timvuc0tc9pg3qd.lan>
2020-06-10 12:51:52 -0400

    ionic-vue test edit

--- test/ionic-vue.spec.ts
+++ test/ionic-vue.spec.ts
@@ -5,14 +5,13 @@ import Ionic from '@ionic/core/loader';
 //does not work yet. 
 describe('test that install functions works', () => {
     let app = jest.fn();
-    it('should test that applyPolyfills & defineCustomElements is called', () => {
+    it('should test that applyPolyfills & defineCustomElements is called', async () => {
         expect(typeof IonicVue.install).toBe('function'); //tests if function exists
-        let spy = jest.spyOn(Ionic, 'applyPolyfills');
-        IonicVue.install(app as any);
-        expect(spy).toHaveBeenCalled(); 
-        const spy2 = jest.spyOn(Ionic, 'defineCustomElements');
-        IonicVue.install(app as any);
-        expect(spy2).toHaveBeenCalled(); 
+        const polyfillSpy = jest.spyOn(Ionic, 'applyPolyfills');
+        const customElementsSpy = jest.spyOn(Ionic, 'defineCustomElements');
+        await IonicVue.install!(app as any);
+        expect(customElementsSpy).toHaveBeenCalled(); 
+        expect(polyfillSpy).toHaveBeenCalled();
     })
 })
 
