7d4962ba373df17c9a923a056dc19ce5ac9bba89
Michael Tintiuc <contact@michaeltintiuc.com>
2020-08-22 11:59:54 +0300

    Ignore unrelated inoic props

--- scripts/vetur.ts
+++ scripts/vetur.ts
@@ -5,7 +5,13 @@ import { paramCase } from 'change-case';
 import { IonRouterView } from '../src/components';
 
 const ignoredProps = ['component', 'componentProps', 'delegate'];
-const ignoredTags = ['ion-nav', 'ion-nav-link', 'ion-router', 'ion-route', 'ion-route-redirect'];
+const ignoredTags = [
+  'ion-nav',
+  'ion-nav-link',
+  'ion-router',
+  'ion-route',
+  'ion-route-redirect',
+];
 const navigableTags = ['href', 'default-href'];
 const tags: { [key: string]: any } = {
   'ion-page': {
@@ -15,7 +21,7 @@ const tags: { [key: string]: any } = {
   'ion-router-view': {
     description:
       '`IonRouterView` is built on top of official `<router-view>` that renders the matched component for the current location with added logic for providing a stacked navigation, animating views in and out and swipe-back gesture. Components rendered by `<ion-router-view>` can also contain their own `<ion-router-view>` to render nested routes and tabbed navigation.',
-    attributes: (IonRouterView.props as string[]).map(p => paramCase(p))
+    attributes: (IonRouterView.props as string[]).map(p => paramCase(p)),
   },
 };
 const attrs: { [key: string]: any } = {
@@ -26,50 +32,62 @@ const attrs: { [key: string]: any } = {
   },
   'ion-router-view/name': {
     type: 'string | symbol',
-    description: 'When a `<ion-router-view>` has a `name` prop, it will render the component with the corresponding name in the matched route record\'s components option.'
+    description:
+      'When a `<ion-router-view>` has a `name` prop, it will render the component with the corresponding name in the matched route record\'s components option.',
   },
   'ion-router-view/route': {
-    description: 'When a `<ion-router-view>` has a `route` prop, it will use that resolved Route Location instead of the current location.'
+    description:
+      'When a `<ion-router-view>` has a `route` prop, it will use that resolved Route Location instead of the current location.',
   },
 };
 
-docs.components.filter(c => !ignoredTags.includes(c.tag)).forEach(c => {
-  let tagName = c.tag;
-  if (tagName === 'ion-router-outlet') {
-    tagName = 'ion-router-view';
-  } else {
-    const description =
-      tagName !== 'ion-router-link'
-        ? c.docs
-        : 'The router link component is used for navigating to a specified link. Similar to the browser\'s anchor tag, it can accept a href for the location, and a direction for the transition animation.';
+docs.components
+  .filter(c => !ignoredTags.includes(c.tag))
+  .forEach(c => {
+    let tagName = c.tag;
+    if (tagName === 'ion-router-outlet') {
+      tagName = 'ion-router-view';
+    } else {
+      const description =
+        tagName !== 'ion-router-link'
+          ? c.docs
+          : 'The router link component is used for navigating to a specified link. Similar to the browser\'s anchor tag, it can accept a href for the location, and a direction for the transition animation.';
 
-    tags[tagName] = {
-      description,
-      attributes: c.props
-        .filter(p => !ignoredProps.includes(p.name))
-        .map(p => p.attr || paramCase(p.name)),
-    };
-  }
-
-  c.props.map(p => {
-    if (navigableTags.includes(p.attr || '') && tagName !== 'ion-tab-button') {
-      tags[tagName].attributes.push('replace');
+      tags[tagName] = {
+        description,
+        attributes: c.props
+          .filter(p => !ignoredProps.includes(p.name))
+          .map(p => p.attr || paramCase(p.name)),
+      };
     }
 
-    const options: string[] = [];
-    p.values.forEach(v => {
-      if (v.value) {
-        options.push(v.value);
-      }
-    });
+    c.props
+      .filter(p => !ignoredProps.includes(p.name))
+      .map(p => {
+        if (
+          navigableTags.includes(p.attr || '') &&
+          tagName !== 'ion-tab-button'
+        ) {
+          tags[tagName].attributes.push('replace');
+        }
 
-    attrs[`${tagName}/${p.attr || paramCase(p.name)}`] = {
-      type: p.type,
-      description: p.docs.replace(/router-outlet|ion-nav/g, 'ion-router-view'),
-      ...(options.length ? { options } : {}),
-    };
+        const options: string[] = [];
+        p.values.forEach(v => {
+          if (v.value) {
+            options.push(v.value);
+          }
+        });
+
+        attrs[`${tagName}/${p.attr || paramCase(p.name)}`] = {
+          type: p.type,
+          description: p.docs.replace(
+            /router-outlet|ion-nav/g,
+            'ion-router-view'
+          ),
+          ...(options.length ? { options } : {}),
+        };
+      });
   });
-});
 
 if (!fs.existsSync(path.resolve(__dirname, '../dist'))) {
   fs.mkdirSync(path.resolve(__dirname, '../dist'));
